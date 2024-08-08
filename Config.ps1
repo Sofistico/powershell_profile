2125eec9f848dfa8c8dfa4e58bd8eea9793567a0
Sofistico <59299346+Sofistico@users.noreply.github.com>
2024-08-08 09:55:26 -0300

    Update Config.ps1

--- Config.ps1
+++ Config.ps1
@@ -1,3 +1,5 @@
 winget install --id Git.Git -e --source winget
 winget install ajeetdsouza.zoxide
 winget install fzf
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
