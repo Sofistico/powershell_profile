8cbb3135dc89b88a9b5855c736b28f7ab3216109
Sofistico <59299346+Sofistico@users.noreply.github.com>
2024-11-22 23:03:19 -0300

    Update Profile.ps1

--- Profile.ps1
+++ Profile.ps1
@@ -1,16 +1,40 @@
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
-# i can't be sure that there will be these instalation in any new machine, so comment it for now
-#function Set-Kickstart {
-#    [Environment]::SetEnvironmentVariable('NVIM_APPNAME', 'nvim')
-#	nvim
-#}
 
-#function Set-Lazy {
-#    [Environment]::SetEnvironmentVariable('NVIM_APPNAME', 'lazy')
-#	nvim
-#}
+# Load work functions
+$wf = "$PSScriptRoot\WorkFunctions.ps1"
+if (Test-Path $wf -ErrorAction SilentlyContinue) {
+  . $wf
+}
 
-#Set-Alias -Name kickstart -Value Set-Kickstart
-#Set-Alias -Name lazy -Value Set-Lazy
+# A shortcut I used in unix regularly
+function ll { Get-ChildItem -Force $args }
+
+# Still allow me to use gco alias
+function Get-GitCheckout {
+  [alias("gco")]
+  param()
+  git checkout $args
+}
+# Regularly used in unix, but now for Windows
+function which { param($bin) Get-Command $bin }
+# Another unix regular that I wanted to replicate for Windows
+function Watch-Command {
+  [alias('watch')]
+  [CmdletBinding()]
+  param (
+    [Parameter()]
+    [ScriptBLock]
+    $Command,
+    [Parameter()]
+    [int]
+    $Delay = 2
+  )
+  while ($true) {
+    Clear-Host
+    Write-Host ("Every {1}s: {0} `n" -F $Command.toString(), $Delay)
+    $Command.Invoke()
+    Start-Sleep -Seconds $Delay
+  }
+}
 
 Set-Alias -Name v -Value nvim
